d9a5fca10b47d19acd6cc2de0f5a457d0c5ff0ff
III-Redbane-III <76516941+III-Redbane-III@users.noreply.github.com>
2021-09-14 03:30:39 +1100

    Add files via upload

---
+++ my-app/src/Components/MessageComponent/Message.js
@@ -0,0 +1,9 @@
+import './MessageStyle.css'
+
+const Message = ({textProp}) => {
+    return (
+        <p className="text">{textProp}</p>
+    )
+};
+
+export default Message;
---
+++ my-app/src/Components/SecondComponent/SecondComponent.js
@@ -0,0 +1,11 @@
+import './SecondComponentStyle.css'
+
+const Second = () => {
+    return (
+        <div className="second">
+            Second
+        </div>
+    )
+};
+
+export default Second;
