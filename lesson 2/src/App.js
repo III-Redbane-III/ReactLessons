71f9c8cbd5f372283bd487bfa027d17e4685f97e
III-Redbane-III <76516941+III-Redbane-III@users.noreply.github.com>
2021-09-17 03:54:53 +1100

    Add files via upload

---
+++ lesson 2/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import MessageList from './Components/messageList/messageList';
+
+
+
+
+
+function App() {
+  return (
+    <>
+    <MessageList />
+    123
+    </>
+  );
+}
+
+export default App;
